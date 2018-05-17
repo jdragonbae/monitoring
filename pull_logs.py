0357ad05b90c3830736f2b8eda42f330a8dacc64
mjkim610 <mjkim610@gmail.com>
2018-05-17 23:20:56 +0900

    Update get_log.yaml

--- pull_logs.py
+++ pull_logs.py
@@ -4,4 +4,4 @@ import subprocess
 servers = ['10.41.2.31', '10.41.1.25', '10.41.5.49', '10.41.2.41', '10.41.0.232', '10.41.5.67', '10.41.2.84', '10.41.0.162', '10.41.5.74']
 
 for server in servers:
-    cmd = subprocess.Popen("sshpass -p gorepdl123 scp hackday@"+server+":hackday.log logs/"+server+".log", shell=True, stdout=subprocess.PIPE)
+    cmd = subprocess.Popen("sshpass -p gorepdl123 scp hackday@"+server+":hackday.log ~/monitoring/logs/"+server+".log", shell=True, stdout=subprocess.PIPE)
