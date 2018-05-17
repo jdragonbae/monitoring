384b0ff2123376761f0b0465cf9bda2a27300064
mjkim610 <mjkim610@gmail.com>
2018-05-18 02:21:40 +0900

    Update analyse_log.py

--- analyze_log.py
+++ analyze_log.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 import pandas
 import numpy
 from scipy import stats
@@ -8,17 +8,21 @@ number_type_logs = ["cpu-0-idle", "cpu-1-idle", "free-mem", "rxpck/s", "txpck/s"
 servers = ['10.41.2.31', '10.41.1.25', '10.41.5.49', '10.41.2.41', '10.41.0.232', '10.41.5.67', '10.41.2.84', '10.41.0.162', '10.41.5.74']
 
 for server in servers:
-	df = pandas.read_csv("logs/"+server+".log", sep=';')
+	if os.path.isfile("hackday_logs/"+server+".log"):
+		df = pandas.read_csv("hackday_logs/"+server+".log", sep=';')
+		df = df.tail(100)
 
-	print "******" + server + "******"
-	for column in df.columns.tolist():
-		if (column in number_type_logs):
-			print "=====" + column + "====="
-			print "mean:\t" + str(df[column].mean())
-			print "median:\t" + str(df[column].median())
-			print "max:\t" + str(df[column].max())
-			print "min:\t" + str(df[column].min())
+		columns = df.columns.tolist()
+		for column in columns:
+			if column not in number_type_logs:
+				df.drop(column, axis=1, inplace=True)
+		
+		print df.describe()
 
-	print df
-	df[(numpy.abs(stats.zscore(df)) < 3).all(axis=1)]
-	print df
+		for col in df.columns.tolist():
+			if (col in ['rxpck/s', 'txpck/s']):
+				if (df[col].std() > df[col].mean()*5):
+					print "possible issue in: server "+server+" col "+col						
+			elif (df[col].std() > df[col].mean()/10):
+				print "possible issue in: server "+server+" col "+col
+		print "============================================"
