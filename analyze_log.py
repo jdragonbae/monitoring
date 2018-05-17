5a282bb5ee9ed2793ac910777f8deb25f5c597a5
mjkim610 <mjkim610@gmail.com>
2018-05-17 23:02:28 +0900

    Update get_log.py

--- analyze_log.py
+++ analyze_log.py
@@ -1,18 +1,16 @@
 import sys
 import pandas
 import numpy
+from scipy import stats
+
 
 number_type_logs = ["cpu-0-idle", "cpu-1-idle", "free-mem", "rxpck/s", "txpck/s"]
-filename = "hackday.log"
+servers = ['10.41.2.31', '10.41.1.25', '10.41.5.49', '10.41.2.41', '10.41.0.232', '10.41.5.67', '10.41.2.84', '10.41.0.162', '10.41.5.74']
 
-def get_df(logfile_name):
-	df = pandas.read_csv(logfile_name, sep=';')
-	return df
+for server in servers:
+	df = pandas.read_csv("logs/"+server+".log", sep=';')
 
-if __name__ == '__main__':
-	
-	df = pandas.read_csv(filename, sep=';')
-
+	print "******" + server + "******"
 	for column in df.columns.tolist():
 		if (column in number_type_logs):
 			print "=====" + column + "====="
@@ -20,3 +18,7 @@ if __name__ == '__main__':
 			print "median:\t" + str(df[column].median())
 			print "max:\t" + str(df[column].max())
 			print "min:\t" + str(df[column].min())
+
+	print df
+	df[(numpy.abs(stats.zscore(df)) < 3).all(axis=1)]
+	print df
--- get_log.py
+++ get_log.py
@@ -2,101 +2,93 @@ import sys
 import os
 import subprocess
 import datetime
+import time
+import threading
 
 
 filename = "hackday.log"
-jenkins_server = "root@10.41.3.244"
 
-if not os.path.isfile(filename):
-	with open(filename, 'w') as writefile:
-		writefile.write("index;")
-		writefile.write("time;")
-		writefile.write("cpu-0-idle;cpu-1-idle;")
-		writefile.write("free-mem;")
-		writefile.write("rxpck/s;txpck/s;")
+def create_file():
+	if not os.path.isfile(filename):
+		with open(filename, 'w') as writefile:
+			writefile.write("index;")
+			writefile.write("time;")
+			writefile.write("cpu-0-idle;cpu-1-idle;")
+			writefile.write("free-mem;")
+			writefile.write("rxpck/s;txpck/s;")
+			
+			writefile.write("\n")
+
+
+def record_new_log():
+
+	line_count = sum(1 for line in open(filename))
+
+	with open(filename, 'a') as appendfile:
 		
-		writefile.write("\n")
-
-line_count = sum(1 for line in open(filename))
-
-with open(filename, 'a') as appendfile:
-	
-	#================
-	# index
-	#=================
-	appendfile.write(str(line_count)+";")
-
-	#================
-	# datetime.now()
-	#=================
-	appendfile.write(str(datetime.datetime.now())+";")
+		#================
+		# index
+		#=================
+		appendfile.write(str(line_count)+";")
 
 
-	#=================
-	# mpstat
-	#=================
-	cmd = subprocess.Popen("mpstat -P ALL", shell=True, stdout=subprocess.PIPE)
-	
-	counter = 0
-	for line in cmd.stdout:
-		counter += 1
-		if counter in [5, 6]:
-			values = line.split()
-			appendfile.write(values[12]+";")
+		#================
+		# datetime.now()
+		#=================
+		appendfile.write(str(datetime.datetime.now())+";")
 
 
-	#=================
-	# /proc/meminfo
-	#=================
-	cmd = subprocess.Popen("cat /proc/meminfo", shell=True, stdout=subprocess.PIPE)
-
-	counter = 0
-	for line in cmd.stdout:
-		counter += 1
-		if counter == 2:
-			values = line.split()
-			appendfile.write(values[1]+";")
-			break
-
-
-	#=================
-	# sar -n
-	#=================
-	cmd = subprocess.Popen("sar -n DEV 1 1", shell=True, stdout=subprocess.PIPE)
-
-	counter = 0
-	for line in cmd.stdout:
-		counter += 1
-		if counter == 8:
-			values = line.split()
-			appendfile.write(values[2]+";"+values[3]+";")
-			break
-	
-	
-	#=================
-	# end logging
-	#=================
-	appendfile.write("\n")
+		#=================
+		# mpstat
+		#=================
+		cmd = subprocess.Popen("mpstat -P ALL", shell=True, stdout=subprocess.PIPE)
+		
+		counter = 0
+		for line in cmd.stdout:
+			counter += 1
+			if counter in [5, 6]:
+				values = line.split()
+				appendfile.write(values[12]+";")
 
 
-"""
-#=================
-# output
-#=================
-with open(filename, 'r') as readfile:
-	for line in readfile:
-		sys.stdout.write(line)
-"""
-#=================
-# output
-#=================
-with open(filename, 'r') as readfile:
-	output = '"'
-	for line in readfile:
-		output += line
-	
-	output += '"'
-	hostname = ""
-	cmd = subprocess.Popen("hostname --ip-address", shell=True, stdout=subprocess.PIPE)
-	hostname = cmd.stdout.readline().rstrip('\n')
-	cmd = subprocess.Popen("echo "+output+" | sshpass -p gorepdl123 ssh "+jenkins_server+" 'cat > "+hostname+".log'", shell=True, stdout=subprocess.PIPE)
+		#=================
+		# /proc/meminfo
+		#=================
+		cmd = subprocess.Popen("cat /proc/meminfo", shell=True, stdout=subprocess.PIPE)
+
+		counter = 0
+		for line in cmd.stdout:
+			counter += 1
+			if counter == 2:
+				values = line.split()
+				appendfile.write(values[1]+";")
+				break
+
+
+		#=================
+		# sar -n
+		#=================
+		cmd = subprocess.Popen("sar -n DEV 1 1", shell=True, stdout=subprocess.PIPE)
+
+		counter = 0
+		for line in cmd.stdout:
+			counter += 1
+			if counter == 8:
+				values = line.split()
+				appendfile.write(values[2]+";"+values[3]+";")
+				break
+		
+		
+		#=================
+		# end logging
+		#=================
+		appendfile.write("\n")
+
+		#=================
+		# loop
+		#=================
+		threading.Timer(10, record_new_log).start()
+
+if __name__ == "__main__":
+	create_file()
+	record_new_log()
