1cc8acbcd7d87f78ba44241caa950b4a9c0d26d8
mjkim610 <mjkim610@gmail.com>
2018-05-18 01:35:53 +0900

    Update server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import urlparse
+import os
 
 
 class Server(BaseHTTPRequestHandler):
@@ -13,12 +14,34 @@ class Server(BaseHTTPRequestHandler):
         self.wfile.write("Works")
 
     def do_POST(self):
+
+        hostname = str(self.client_address[0])
+        filename = "hackday_logs/"+hostname+".log"
+
         length = int(self.headers.getheader('content-length'))
         field_data = self.rfile.read(length)
         fields = urlparse.parse_qs(field_data)
-        for field in fields:
-            print field
-        filename = str(self.client_address[0]) + ".log"
+
+    	if not os.path.isfile(filename):
+    		with open(filename, 'w') as writefile:
+            writefile.write("index;")
+            writefile.write("time;")
+            writefile.write("cpu-0-idle;cpu-1-idle;")
+            writefile.write("free-mem;")
+            writefile.write("rxpck/s;txpck/s;")
+            writefile.write("\n")
+
+        with open(filename, 'a') as appendfile:
+            appendfile.write(fields.get("index")[0]+";")
+            appendfile.write(fields.get("time")[0]+";")
+            appendfile.write(fields.get("cpu-0-idle")[0]+";")
+            appendfile.write(fields.get("cpu-1-idle")[0]+";")
+            appendfile.write(fields.get("free-mem")[0]+";")
+            appendfile.write(fields.get("rxpck/s")[0]+";")
+            appendfile.write(fields.get("txpck/s")[0]+";")        
+            appendfile.write("\n")
+        print "POST received."
+        
 
 def run(server_class=HTTPServer, handler_class=Server, port=80):
     server_address = ('', port)
